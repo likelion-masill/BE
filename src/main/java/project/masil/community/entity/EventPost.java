7df6d0b730a40c27b563407bc9ab7c75c32b83d0
bikooju <jhjk1234@gmail.com>
2025-08-09 02:23:44 +0900

    :sparkles: 이벤트 이미지 삭제

--- src/main/java/project/masil/community/entity/EventPost.java
+++ src/main/java/project/masil/community/entity/EventPost.java
@@ -1,7 +1,9 @@
 package project.masil.community.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,6 +14,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,15 +44,17 @@ public class EventPost extends Post {
   @Column(nullable = false)
   private LocalDateTime endAt;
 
-  @Column(nullable = false)
+  @Column(name = "summary")
   private String summary;
 
   @Column(nullable = false)
   private int viewCount = 0;
 
-  @OneToMany(mappedBy = "eventPost", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-  @OrderColumn(name = "sequence") // 순서 유지
-  private List<EventImage> images;
+  @ElementCollection
+  @CollectionTable(name = "event_images", joinColumns = @JoinColumn(name = "event_id"))
+  @Column(name = "image_url", nullable = false)         // 값 컬럼 이름 지정
+  @OrderColumn(name = "sequence")                       // 순서(인덱스) 컬럼 저장
+  private List<String> eventImages = new ArrayList<>();
 
   @OneToMany(mappedBy = "eventPost", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
   private List<ClubPost> clubPosts;
