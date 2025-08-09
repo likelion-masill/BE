cb1e02e93a01df606d590e4124e41df90222ddb7
jaeyeon <kasto6329@gmail.com>
2025-08-09 17:37:11 +0900

    🐛 Fix: Security 관련 문제 해결

--- src/main/java/project/masil/global/config/SecurityConfig.java
+++ src/main/java/project/masil/global/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,27 +31,26 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         // CORS 설정 활성화(보통은 CORS 설정 활성화 하지 않음. 서버에서 NginX로 CORS 검증)
         .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-        // HTTP Basic 인증 기본 설정
-//        .httpBasic(Customizer.withDefaults())
-        // ★ Basic/폼 로그인 완전 비활성화 → 브라우저 팝업 방지
+        // 기본 인증(HTTP Basic)과 폼 로그인 비활성화
+        // → 브라우저 팝업 및 기본 로그인 페이지 차단, JWT 인증만 사용
         .httpBasic(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         // 세션을 생성하지 않음 (JWT 사용으로 인한 Stateless 설정)
         .sessionManagement(
-        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         // HTTP 요청에 대한 권한 설정
         .authorizeHttpRequests(
             request ->
                 request
-                    // Swagger 경로 인증 필요
+                    // Swagger 경로 인증 허용
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
                     .permitAll()
                     // 인증 없이 허용할 경로
-                    .requestMatchers("/api/**")
+                    .requestMatchers("/users/sign-up", "/auths/login")
                     .permitAll()
                     // 그 외 모든 요청은 모두 인증 필요
                     .anyRequest()
-                    .permitAll()) //수정 필요
+                    .authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
