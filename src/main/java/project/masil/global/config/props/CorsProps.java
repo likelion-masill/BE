f48794ce4ee08895f536ea8404c697b874f7d6e9
jaeyeon <kasto6329@gmail.com>
2025-08-22 03:02:04 +0900

    🔧 Settings: Cors Origin 추가

--- src/main/java/project/masil/global/config/props/CorsProps.java
+++ src/main/java/project/masil/global/config/props/CorsProps.java
@@ -1,5 +1,6 @@
 package project.masil.global.config.props;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,9 +11,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "cors")
 public class CorsProps {
 
-  private List<String> allowedOrigins = List.of();
-  private List<String> allowedMethods = List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
-  private List<String> allowedHeaders = List.of("*");
+  private List<String> allowedOrigins = new ArrayList<>();
+  private List<String> allowedMethods = new ArrayList<>(
+      List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+  private List<String> allowedHeaders = new ArrayList<>(List.of("*"));
+  private List<String> exposedHeaders = new ArrayList<>();
   private Boolean allowCredentials = true;
 
 }
