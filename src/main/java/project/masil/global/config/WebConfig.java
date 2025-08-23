7ab219aebd5284b4b70d234f8a31851ca2d8eee0
jaeyeon <kasto6329@gmail.com>
2025-08-23 18:20:25 +0900

    ✨ Feat: 서비스/클라이언트 실행 시간 로깅 AOP 추가

--- src/main/java/project/masil/global/config/WebConfig.java
+++ src/main/java/project/masil/global/config/WebConfig.java
@@ -2,17 +2,30 @@ package project.masil.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.masil.global.converter.MultipartJackson2HttpMessageConverter;
+import project.masil.global.log.RequestTimingInterceptor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+  private final RequestTimingInterceptor timingInterceptor;
+
 
   @Override
   public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
     converters.add(new MultipartJackson2HttpMessageConverter(new ObjectMapper()));
   }
 
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(timingInterceptor)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/health", "/actuator/**", "/static/**");
+  }
 }
